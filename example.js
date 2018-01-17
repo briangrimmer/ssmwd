04f7e3b5fe8b25cd39b3f823e7e8e815cdd3600b
Brian Grimmer <grimmerb@cwu.edu>
2018-01-17 00:04:16 -0800

    Initial commit

---
+++ example.js
@@ -0,0 +1,4 @@
+    function greet() {
+        console.log('Hey from SKillshare');        
+    }
+    export default greet;
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entry: './app.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    devServer: {
+        port: 3000
+    }
+
+};
